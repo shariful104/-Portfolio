67a08cf744db81b0059530113bc4a848a129f7ba
shariful104 <sharifulpc04@gmail.com>
2024-12-14 08:13:33 +0600

    class 3 home work

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("Hello! I am shariful.")
+
+const multi =() =>{
+    console.log(5*5)
+}
+multi();
+
+const add =() =>{
+    console.log(5+5)
+}
+add();
+
+const sub =() =>{
+    console.log(10-5)
+}
+sub();
+
+
+const div =() =>{
+    console.log(15/5)
+}
+div();
+
+
+const myAlert =() =>{
+    alert("This is my alert")
+   
+}
+
+const data =() =>{
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+}
